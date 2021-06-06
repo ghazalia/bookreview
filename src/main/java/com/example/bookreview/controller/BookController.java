e83b19c9d1aafb60737d5e28f62239e93f5194b5
Ghazali <mghazk@yahoo.com>
2021-06-07 02:04:12 +0800

    remove unused logger

--- src/main/java/com/example/bookreview/controller/BookController.java
+++ src/main/java/com/example/bookreview/controller/BookController.java
@@ -1,6 +1,5 @@
 package com.example.bookreview.controller;
 
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.bookreview.exception.DataAlreadyExistException;
 import com.example.bookreview.exception.DataNotFoundException;
@@ -19,7 +17,6 @@ import com.example.bookreview.service.NovelService;
 
 @Controller
 public class BookController {
-	private Logger log = org.slf4j.LoggerFactory.getLogger(BookController.class);
 
 	@Autowired
 	private NovelService novelService;
@@ -28,9 +25,7 @@ public class BookController {
 	private AuthorService authorService;
 
 	@GetMapping("novel/new/{authorId}")
-	public String newBook(@PathVariable("authorId") Long authorId,
-						  Novel novel,
-						  Model model) {
+	public String newBook(@PathVariable("authorId") Long authorId, Novel novel, Model model) {
 		try {
 			Author author = authorService.getAuthorById(authorId);
 			novel.setAuthor(author);
@@ -45,9 +40,7 @@ public class BookController {
 	}
 
 	@PostMapping("novel/new/{authorId}")
-	public String save(@PathVariable("authorId") Long authorId,
-					   @ModelAttribute Novel novel,
-					   Model model) {
+	public String save(@PathVariable("authorId") Long authorId, @ModelAttribute Novel novel, Model model) {
 		try {
 			Author author = authorService.getAuthorById(authorId);
 			novel.setAuthor(author);
